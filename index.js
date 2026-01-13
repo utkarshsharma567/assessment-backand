2351a826fe77dbc3aeca12483fa42cd24668f981
utkarshsharma567 <us80999@gmail.com>
2026-01-13 11:23:49 +0530

    Refactor CORS middleware to support multiple origins

--- index.js
+++ index.js
@@ -18,7 +18,23 @@ const port = process.env.PORT || 4000;
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://assessment-frontened.onrender.com"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 // MongoDB connection
   try {
